900a211fb923972347ddca0c55884e23886521d5
daidong <daidong@zmeng123.com>
2017-12-15 22:41:28 +0800

    add commit

--- src/ODPS/Services/InstanceService.php
+++ src/ODPS/Services/InstanceService.php
@@ -58,8 +58,24 @@ class InstanceService extends \ODPS\Core\OdpsBase
         return $this->call($options);
     }
 
-    public function getInstances($daterange = null, $status = null, $jobname = null, $onlyowner = null)
-    {
+    /**
+     * 查看所有 instance 信息
+     * @param $daterange    指定查询instance 开始运行的时间范围。格式为：daterange=[n1]:[n2] ，
+     *                          其中n1是指时间范围的开始，n2为结束。n1和n2是将时间日期转换成整型后的数值。
+     *                          省略n1，查询条件为截止到n2,格式为:n2；省略n2，查询条件为从n1开始截到现在，格式为 n1:；
+     *                          同时省略n1和n2等同于忽略daterange查询条件
+     * @param $status       instance的状态，有效值包括：Running,Suspended,Terminated，可省
+     * @param $jobname      用于指定通过SubJob task 启动的Job名称，可省
+     * @param $onlyowner    指示是否只返回提交查询人自己的instance。有效值包括：yes，no。yes: 只返回自己的，no返回所有的。 参数可省，默认为yes.
+     * # @param $marker     指定instance的标识，在返回记录时会从指定标识开始返回记录。
+     *                          如果没有指定这个参数，会从第1条返回。
+     *                          在API的response中，会返回下一次开始的标识，指定的marker值无效，返回0条记录；
+     * # @param maxItems    指定本次查询期待返回最大的记录数，默认为1000。 
+     *                          * 如果符合条件的记录小于maxitems，在API 的Response中会返回本次请求中准确的Maxitems； 
+     * 
+     * @return OdpsIterator object
+     * */
+    public function getInstances($daterange = null, $status = null, $jobname = null, $onlyowner = null) {
         return new OdpsIterator(
             array(
                 "obj" => $this,
@@ -76,33 +92,33 @@ class InstanceService extends \ODPS\Core\OdpsBase
     }
 
     /**
-    * 查看 instance 中 task 状态
-    * @param $instancename 请求的 instance 名称
-    * @return $taskStatus = $instance->getInstanceTask($instanceId)->Tasks->Task->Status;
-    *   XML: 自行处理 
-    *   <?xml version="1.0" encoding="UTF-8"?>
-    *       <Instance>
-    *           <Status>Terminated</Status>
-    *           <Tasks>
-    *               <Task Type="SQL">
-    *               <Name>@task</Name>
-    *               <StartTime>Fri, 15 Dec 2017 14:02:04 GMT</StartTime>
-    *               <EndTime>Fri, 15 Dec 2017 14:02:16 GMT</EndTime>
-    *               <Status>Success</Status>
-    *               <Histories/>
-    *               </Task>
-    *           </Tasks>
-    *       </Instance>
-    *   参数        描述
-    *   Instance    Instance 描述
-    *   Status      Instance 状态
-    *   Tasks       Instance 中 task 列表描述
-    *   Task        Task 描述
-    *   Name        Task 名称
-    *   StartTime   Task 开始时间
-    *   EndTime     Task 结束时间 
-    *   Status      Task 状态
-    */
+     * 查看 instance 中 task 状态
+     * @param $instancename 请求的 instance 名称
+     * @return $taskStatus = $instance->getInstanceTask($instanceId)->Tasks->Task->Status;
+     *   XML: 自行处理 
+     *   <?xml version="1.0" encoding="UTF-8"?>
+     *       <Instance>
+     *           <Status>Terminated</Status>
+     *           <Tasks>
+     *               <Task Type="SQL">
+     *               <Name>@task</Name>
+     *               <StartTime>Fri, 15 Dec 2017 14:02:04 GMT</StartTime>
+     *               <EndTime>Fri, 15 Dec 2017 14:02:16 GMT</EndTime>
+     *               <Status>Success</Status>
+     *               <Histories/>
+     *               </Task>
+     *           </Tasks>
+     *       </Instance>
+     *   参数        描述
+     *   Instance    Instance 描述
+     *   Status      Instance 状态
+     *   Tasks       Instance 中 task 列表描述
+     *   Task        Task 描述
+     *   Name        Task 名称
+     *   StartTime   Task 开始时间
+     *   EndTime     Task 结束时间 
+     *   Status      Task 状态
+     */
     public function getInstanceTask($instancename) {
         $resource = ResourceBuilder::buildInstanceUrl($this->getDefaultProjectName(), $instancename);
         $options = array(
